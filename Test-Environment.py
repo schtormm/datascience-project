3a538dc2b87bd7a1aa9c60b3ed1bdefc494ac833
schtormm <storm.noordermeer@gmail.com>
2025-12-11 22:26:05 +0100

    include multiple ARIMA orders, try to accomodate different models

--- Test-Environment.py
+++ Test-Environment.py
@@ -47,102 +47,133 @@ def create_dataset(countries, test_period, dataset):
         country_dfs[country] = {'train': train, 'test': test}
     return country_dfs
 
-def create_models(country_dfs, model, parameters):
-    arima_order = parameters['orders'][0]  # Example: using the first ARIMA order
+def create_models_ARIMA(country_dfs, model, parameters):
+    arima_orders = parameters['orders']  # Example: using the first ARIMA order
     # For each country, create and fit an ARIMA model and save it in a dictionary
     country_models = {}
-    print(f"Creating models with order {arima_order} for countries:", list(country_dfs.keys()))
+    print("Creating models for countries:", list(country_dfs.keys()))
     for country, data in country_dfs.items():
-        print(f"Fitting ARIMA model for {country} with order {arima_order}")
-        model = ARIMA(data['train'], order=(2, 1, 1)).fit()
-        country_models[country] = model
+        country_models[country] = []
+        for order in arima_orders:
+            print(f"Fitting ARIMA{order} model for {country}")
+            model_fit = ARIMA(data['train'], order=order).fit()
+            country_models[country].append(model_fit)
 
     return country_models
 
-def evaluate_models(country_dfs, model):
+def evaluate_models_arima(country_dfs, models):
     # get MSE, RMSE and MAPE for each model on its test set using crossvalidation
-    # should we possibly also use the different ARIMA forms here @Wesley1701?
 
     # note for MAPE from sklearn: 
     # Note that we are not using the common “percentage” definition: the percentage in the range [0, 100] is converted to a relative value in the range [0, 1] by dividing by 100. 
     # Thus, an error of 200% corresponds to a relative error of 2.
     evaluation_metrics = {}
     for country, data in country_dfs.items():
+        evaluation_metrics[country] = []
         test = data['test']
-        model_fit = model[country]
-        forecast = model_fit.predict(start=test.index[0], end=test.index[-1])
-        mape = mean_absolute_percentage_error(test, forecast)
-        mse = mean_squared_error(test, forecast)
-        rmse = root_mean_squared_error(test, forecast)
-        evaluation_metrics[country] = {'MSE': mse, 'RMSE': rmse, 'MAPE': mape}
+        models_to_evaluate = models[country]
+        for model_fit in models_to_evaluate:
+            forecast = model_fit.predict(start=test.index[0], end=test.index[-1])
+            order = model_fit.model.order
+            mape = mean_absolute_percentage_error(test, forecast)
+            mse = mean_squared_error(test, forecast)
+            rmse = root_mean_squared_error(test, forecast)
+            evaluation_metrics[country].append({'ARIMA Order': order, 'MSE': mse, 'RMSE': rmse, 'MAPE': mape})
+        
 
     eval_df = pd.DataFrame.from_dict(evaluation_metrics, orient='index')
     eval_df.to_csv('evaluation_metrics.csv')
 
 
-def evaluate_models_cross(country_dfs, model):
+def evaluate_models_cross_ARIMA(country_dfs, models):
     # get MSE, RMSE and MAPE for each model on its test set using crossvalidation
     # same notes as above
     evaluation_metrics_cross = {}
     for country, data in country_dfs.items():
+        evaluation_metrics_cross[country] = []
         series = data['train']._append(data['test'])
-        model_fit = model[country]
         n_splits = 5
-        mse_list = []
-        rmse_list = []
-        mape_list = []
         split_size = len(series) // n_splits
-        for i in range(n_splits):
-            train_end = (i + 1) * split_size
-            train = series[:train_end]
-            test = series[train_end:train_end + split_size]
-            if len(test) == 0:
-                continue
-            model_cv = ARIMA(train, order=(2, 1, 1)).fit()
-            forecast = model_cv.predict(start=test.index[0], end=test.index[-1])
-            mse_list.append(mean_squared_error(test, forecast))
-            rmse_list.append(root_mean_squared_error(test, forecast))
-            mape_list.append(mean_absolute_percentage_error(test, forecast))
-        evaluation_metrics_cross[country] = {
-            'MSE': np.mean(mse_list),
-            'RMSE': np.mean(rmse_list),
-            'MAPE': np.mean(mape_list)
-        }
+        for model in models[country]:
+            mse_list = []
+            rmse_list = []
+            mape_list = []
+            for i in range(n_splits):
+                train_end = (i + 1) * split_size
+                train = series[:train_end]
+                test = series[train_end:train_end + split_size]
+                if len(test) == 0:
+                    continue
+                
+                try:
+                    model_cv = ARIMA(train, order=model.model.order).fit()
+                    forecast = model_cv.predict(start=test.index[0], end=test.index[-1])
+                    mse_list.append(mean_squared_error(test, forecast))
+                    rmse_list.append(root_mean_squared_error(test, forecast))
+                    mape_list.append(mean_absolute_percentage_error(test, forecast))
+                except Exception as e:
+                    print(f"Warning: ARIMA model cross validation failed for {country} with order {model.model.order} on split {i}: {e}")
+                    continue
+            
+            if mse_list:  # Only add metrics if we have valid results
+                evaluation_metrics_cross[country].append ({
+                    "ARIMA Order": model.model.order,
+                    'MSE (mean of all splits)': np.mean(mse_list),
+                    'RMSE (mean of all splits)': np.mean(rmse_list),
+                    'MAPE (mean of all splits)': np.mean(mape_list)
+                })
    
     eval_df = pd.DataFrame.from_dict(evaluation_metrics_cross, orient='index')
-    eval_df.to_csv('evaluation_metrics.csv')
+    eval_df.to_csv('evaluation_metrics_cross.csv')
 
 
 
-def create_plots(models, country_dfs, timerange, forecast_horizon):
-    print(f'Models: {json.dumps(models, indent=4, default=str)}')
+def create_plots_arima(models, country_dfs, timerange, forecast_horizon):
+    
     for country in models.keys():
-        plt.figure(figsize=(10, 6))
+        fig, axs = plt.subplots(3, 1, figsize=(10, 20))
         model = models[country]
-        plot_predict(model, start=timerange[1], end=(timerange[1] + forecast_horizon), ax=None)
-        country_dfs[country]['train'].plot(label='Observed')
-        # make sure plot only shows from 2000 onwards
-        plt.xlim([timerange[0], timerange[1] + forecast_horizon])
-        # get rid of 1e6 notation on y-axis
-        plt.ticklabel_format(style='plain', axis='y')
-        # add title and labels
-        plt.title(f'ARIMA(2,1,1) Forecast of rgdpe for {country}')
-        plt.xlabel('Year')
-        plt.ylabel('Real GDP (in millions)')
-        plt.legend()
-        # Save the plot to a file
-        plot_name = f'plots_{country}.png'
-        plt.savefig(plot_name, dpi=300, bbox_inches='tight')
-        plt.close()
+        # create one plot
+        i = 0
+        for m in model:
+            # put plot in the figure
+            forecast = m.get_forecast(steps=forecast_horizon)
+            forecast_index = np.arange(timerange[1], timerange[1] + forecast_horizon)
+            # subplot for each model
+            axs[i].plot(forecast_index, forecast.predicted_mean, label=f'Forecast ARIMA{m.model.order}')
+            country_dfs[country]['train'].plot(ax=axs[i], label='Observed')
+            # make sure plot only shows from 2000 onwards
+            axs[i].set_xlim([timerange[0], timerange[1] + forecast_horizon])
+            # get rid of 1e6 notation on y-axis
+            axs[i].ticklabel_format(style='plain', axis='y')
+            # add title and labels
+            axs[i].set_title(f'ARIMA{m.model.order} Forecast of rgdpe for {country}')
+            axs[i].set_xlabel('Year')
+            axs[i].set_ylabel('Real GDP (in millions)')
+            axs[i].legend()
+            i +=1
+            # Save the plot to a file
+            plot_name = f'plots_{country}.png'
+        fig.savefig(plot_name, dpi=300, bbox_inches='tight')
+        
         print(f"Plot saved for {country}: {plot_name}")  
 
 if __name__ == "__main__":
     parameters = get_parameters()
     print(f"Parameters: {parameters}")
     country_dfs = create_dataset(parameters['country_codes'], parameters['test_period'], dataset)
-    # arima_order = parameters['parameters']['orders'][0]  # Example: using the first ARIMA order
-    country_models = create_models(country_dfs, parameters['model'], parameters['parameters'])
-    create_plots(country_models, country_dfs, parameters['test_period'], parameters['forecast_horizon'])
-    evaluate_models(country_dfs, country_models)
-    evaluate_models_cross(country_dfs, country_models)
+
+    #maybe move this into the switch statement
+    country_models = create_models_ARIMA(country_dfs, parameters['model'], parameters['parameters'])
+    match parameters['model']:
+        case 'ARIMA':
+            create_plots_arima(country_models, country_dfs, parameters['test_period'], parameters['forecast_horizon'])
+            evaluate_models_arima(country_dfs, country_models)
+            evaluate_models_cross_ARIMA(country_dfs, country_models)
+        case _:
+            raise ValueError(f"Model {parameters['model']} not recognized.")
+    
+    
+
+
  
