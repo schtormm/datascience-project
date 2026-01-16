8e6aae865210a58db294c3bc29d19d3b960d9650
schtormm <storm.noordermeer@gmail.com>
2026-01-16 15:56:22 +0100

    neevermind

--- Test-Environment.py
+++ Test-Environment.py
@@ -113,43 +113,43 @@ def create_models_linear(country_dfs, parameters):
     return country_models
 
 
-def diagnostic_analysis_pre_prediction(country_dfs, gdp_used, output_folder='experiments'):
-   # create diagnostics subfolder under output_folder
-    os.makedirs(f'{output_folder}/diagnostics', exist_ok=True)
+# def diagnostic_analysis_pre_prediction(country_dfs, gdp_used, output_folder='experiments'):
+#    # create diagnostics subfolder under output_folder
+#     os.makedirs(f'{output_folder}/diagnostics', exist_ok=True)
     
-    for country, data in country_dfs.items():
-        # Combine train and test data
-        full_data = data['train']._append(data['test'])
+#     for country, data in country_dfs.items():
+#         # Combine train and test data
+#         full_data = data['train']._append(data['test'])
         
-        # Create diagnostic plots
-        fig, axes = plt.subplots(1, 3, figsize=(12, 4))
+#         # Create diagnostic plots
+#         fig, axes = plt.subplots(1, 3, figsize=(12, 4))
         
-        # 1. Time series plot
-        axes[0].plot(full_data.index, full_data.values)
-        axes[0].set_title(f'Time Series - {country}')
-        axes[0].set_xlabel('Year')
-        axes[0].set_ylabel(f"{'log of Real GDP' if gdp_used == 'log' else 'Real GDP'}")
+#         # 1. Time series plot
+#         axes[0].plot(full_data.index, full_data.values)
+#         axes[0].set_title(f'Time Series - {country}')
+#         axes[0].set_xlabel('Year')
+#         axes[0].set_ylabel(f"{'log of Real GDP' if gdp_used == 'log' else 'Real GDP'}")
         
-        # 2. QQ-plot
-        qqplot(full_data.values, line='s', ax=axes[1])
-        axes[1].set_title(f'Q-Q Plot - {country}')
+#         # 2. QQ-plot
+#         qqplot(full_data.values, line='s', ax=axes[1])
+#         axes[1].set_title(f'Q-Q Plot - {country}')
         
-        # 3. Histogram
-        axes[2].hist(full_data.values, bins=20, edgecolor='black')
-        axes[2].set_title(f'Distribution - {country}')
-        axes[2].set_xlabel(f"{'log of Real GDP' if gdp_used == 'log' else 'Real GDP'}")
-        axes[2].set_ylabel('Frequency')
+#         # 3. Histogram
+#         axes[2].hist(full_data.values, bins=20, edgecolor='black')
+#         axes[2].set_title(f'Distribution - {country}')
+#         axes[2].set_xlabel(f"{'log of Real GDP' if gdp_used == 'log' else 'Real GDP'}")
+#         axes[2].set_ylabel('Frequency')
         
-        plt.tight_layout()
-        plt.savefig(f'{output_folder}/diagnostics/raw_data_diagnostics_{country}.png', dpi=300)
-        plt.close()
+#         plt.tight_layout()
+#         plt.savefig(f'{output_folder}/diagnostics/raw_data_diagnostics_{country}.png', dpi=300)
+#         plt.close()
         
-        # Shapiro-Wilk test for normality
-        shapiro_test = stats.shapiro(full_data.values)
-        with open(f'{output_folder}/diagnostics/shapiro_wilk_{country}.txt', 'w') as f:
-            f.write(f"Shapiro-Wilk test results for {country}:\n")
-            f.write(f"Statistic: {shapiro_test.statistic}, p-value: {shapiro_test.pvalue}\n")
-            f.close()
+#         # Shapiro-Wilk test for normality
+#         shapiro_test = stats.shapiro(full_data.values)
+#         with open(f'{output_folder}/diagnostics/shapiro_wilk_{country}.txt', 'w') as f:
+#             f.write(f"Shapiro-Wilk test results for {country}:\n")
+#             f.write(f"Statistic: {shapiro_test.statistic}, p-value: {shapiro_test.pvalue}\n")
+#             f.close()
 
 def get_predictions(country_dfs, models, forecast_horizon = None, is_exponential = False):
     predictions = {}
@@ -746,7 +746,7 @@ if __name__ == "__main__":
     match parameters['model']:
         case 'ARIMA':
             country_models = create_models_ARIMA(country_dfs, parameters['parameters'])
-            diagnostic_analysis_pre_prediction(country_dfs, output_folder=output_folder)
+            # diagnostic_analysis_pre_prediction(country_dfs, parameters['gdp_used'], output_folder=output_folder)
             predictions = get_predictions(country_dfs, country_models, forecast_horizon=parameters['forecast_horizon'])
             # print(predictions)
             create_plots_arima(predictions, country_dfs, parameters['test_period'], parameters['forecast_horizon'], output_folder=output_folder)
